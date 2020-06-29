04ce65b521ac1b6c465f92032053cd3b6b9f4beb
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-06-29 21:10:07 +0530

    More directories added

--- javascript/master.js
+++ javascript/master.js
@@ -1,4 +1,7 @@
-let	nameUser = document.getElementById('enterButton').value
+let	nameUser
+let commandHeader
+let commandBox
+console.log(nameUser)
 document.getElementById('enterButton').addEventListener('keyup', () => {
 	if (event.keyCode === 13) {
 		// event.preventDefault();
@@ -23,6 +26,10 @@ document.getElementById('enterButton').addEventListener('keyup', () => {
 		}, 13000);
 	}
 	// console.log(document.getElementById('enterButton').value)
+	
+    nameUser = document.getElementById('enterButton').value
+
+
 	document.getElementById('userName').innerHTML = nameUser
 	document.getElementById('commandButton').focus()
 	document.getElementById('commandButton').onblur = function (event) { 
@@ -31,9 +38,35 @@ document.getElementById('enterButton').addEventListener('keyup', () => {
 			    blurEl.focus()
 			}, 10);
 		};
-})
-let commandBox = `<span class="commandHeader"><span id="userName">${nameUser}</span><span style="color: #ff5680; font-weight: bold;">@</span><span style="color:#d57bff ;">Shreyans</span>:/PORTFOLIO$ -> </span><input id="commandButton" type="text" name="command" autocomplete="false" placeholder="Enter command" onkeyup="caller(event)"><div class="output"></div>`
+    // let commandBox = `<span class="commandHeader"><span id="userName">${nameUser}</span><span style="color: #ff5680; font-weight: bold;">@</span><span style="color:#d57bff ;">Shreyans</span>:/PORTFOLIO$ -> </span><input id="commandButton" type="text" name="command" autocomplete="false" placeholder="Enter command" onkeyup="caller(event)"><div class="output"></div>`
+	commandHeader = `<span class="commandHeader"><span id="userName">${nameUser}</span><span style="color: #ff5680; font-weight: bold;">@</span><span style="color:#d57bff ;">Shreyans</span>:/PORTFOLIO$ `
+// let commandDirectory = `/<span></span>`
 
+	input = `-> </span></span><input id="commandButton" type="text" name="command" autocomplete="false" placeholder="Enter command" onkeyup="caller(event)"><div class="output"></div>`
+
+	commandBox = commandHeader + input
+console.log(commandBox);
+
+})
+
+function onblur (event) { 
+	let blurEl = this; 
+	setTimeout(function() {
+	    blurEl.focus()
+	}, 10);
+};
+
+// // let commandBox = `<span class="commandHeader"><span id="userName">${nameUser}</span><span style="color: #ff5680; font-weight: bold;">@</span><span style="color:#d57bff ;">Shreyans</span>:/PORTFOLIO$ -> </span><input id="commandButton" type="text" name="command" autocomplete="false" placeholder="Enter command" onkeyup="caller(event)"><div class="output"></div>`
+// let commandHeader = `
+// <span class="commandHeader">
+// 	<span id="userName">${nameUser}</span>
+// 	<span style="color: #ff5680; font-weight: bold;">@</span><span style="color:#d57bff ;">Shreyans</span>:/PORTFOLIO$ `
+// // let commandDirectory = `/<span></span>`
+
+// let input = `-> </span></span><input id="commandButton" type="text" name="command" autocomplete="false" placeholder="Enter command" onkeyup="caller(event)"><div class="output"></div>`
+
+// let commandBox = commandHeader + input
+console.log(commandBox);
 // let button = document.querySelectorAll('#commandButton')
 // for(let i = 0 ; i < button.length ; i++) {
 // 	button[i].addEventListener('keyup', () => {
@@ -59,6 +92,82 @@ let commandBox = `<span class="commandHeader"><span id="userName">${nameUser}</s
 // 	// console.log(document.querySelectorAll('#username')[document.querySelectorAll('#username').length -1 ].innerHTML)
 // 	// document.querySelectorAll('#username')[document.querySelectorAll('#username').length -1 ].innerHTML = document.getElementById('enterButton').value
 // }		
+
+let iC = false
+let iP = false
+let iPW = false
+let cdCommand = (command, output) => {
+	if (command.split(' ')[1] == 'BIO'){
+		output.innerHTML = `<p class="bio">
+				<span style='font-weight: bold; color: #f600f6'> Shreyans Jain </span> | Shreyans13 <br>
+				I am a student at <span style='color: #ffa500'>Chandigarh University</span> pursuing <span style='color: #4d4dff'>Bachelor of Technology</span> in Computer Science in Engineering.
+				I <span style='color: #ff1a1a'>love coding</span> and i am <span style='color: #ff3333'>passionate</span> to learn <span 'style: color:#00ff9c'>new</span> things. 
+				I also take part in various <span style='color: #00ccff'>coding competitions</span> and <span style='color: #00ccff'>hackathons.</span>
+				<br>
+				I may not be able to solve <strike>all</strike> <span style='color: #ffff4d'>some</span> of your problems.
+			</p> ${commandBox}`
+		} else if (command.split(' ')[1] == 'CERTIFICATES'){
+			commandBox = commandHeader + `<span>/Certificates/</span>` + input 
+			iC = true
+			output.innerHTML = `${commandBox}`
+		} else if (command.split(' ')[1] == 'SKILLS') {
+			output.innerHTML = `<div class="skills" >
+			<table >
+				<tr>
+					<th>HTML5</th>
+					<td>
+						<div class='progress'>
+							<div class="progress-bar" style="width: 80%; background-color: #f2d32C;">
+							</div>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th>CSS (SASS/SCSS)</th>
+					<td>
+						<div class='progress'>
+							<div class="progress-bar" style="width: 75%; background-color: #f2d31b;">
+							</div>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<th>Javascript</th>
+					<td>
+						<div class='progress'>
+							<div class="progress-bar" style="width: 85%; background-color: #86e01e;">
+							</div>
+						</div>
+					</td>
+				</tr>
+			</table>
+			</div> ${commandBox}`
+		} else if (command.split(' ')[1] == 'PROJECTS') {
+			commandBox = commandHeader + '<span>/Projects/</span>' + input
+			iP = true
+			output.innerHTML = `${commandBox}`
+		} else if (command.split(' ')[1] == 'PROJECTSINWORK') {
+			commandBox = commandHeader + '<span>/ProjectsInWork/</span>' + input
+			iPW = true
+			output.innerHTML = `${commandBox}`
+		} else if (command.split(' ')[1] == '..') {
+			commandBox = commandHeader + input 
+			iC = false
+			iP = false
+			iPW = false
+			output.innerHTML = `${commandBox}`
+		} else if (command.split(' ')[1] == 'CONTACT') {
+			output.innerHTML = `<div class='contact'>Like my Work ? Get in touch ?<br>
+			<a href='' style='margin: 50px'><img src="./svg icons/github.svg"></a>
+			<a href='' style='margin: 50px'><img src="./svg icons/linkedin.svg"></a><br>
+			</div>
+			${commandBox}`
+		} else if (command.split(' ')[1] == 'SENDAMESSAGE') {
+			output.innerHTML = `${commandBox}`
+		} else {
+			output.innerHTML = `<p class="error">-portfolio: Directory: not found </p> ${commandBox}`	
+		}
+}
 let caller = (event) => {
 	if(event.keyCode === 13) {
 		// let ele = document.getElementById('commandButton')
@@ -77,28 +186,100 @@ let caller = (event) => {
 		let output = document.querySelectorAll('.output')[document.querySelectorAll('.output').length -1]
 		console.log(output)
 
-		command = command.toUpperCase()
-		if (command == 'CD BIO') {
-			output.innerHTML = `<p class="bio">
-				<span style='font-weight: bold;'> Shreyans Jain </span> | Shreyans13 <br>
-				I am a student at Chandigarh University pursuing Bachelor of Technology in Computer Science in Engineering.
-				I love coding and i am passionate to learn new things. I also take part in various coding competitions and hackathons.
-				<br>
-				I may not be able to solve <strike>all</strike> some of your problems.
-			</p> ${commandBox}`
-		} else if (command == 'LS' || command == 'DIR') {
-			output.innerHTML = `<p class="ls">
-			<div style="color: #fffc58; padding-left: 45px; line-height: normal;">
-				<p>bio</p>
-				<p>/certificates</p>
-				<p>skills</p>
-				<p>/projects</p>
-				<p>/projectsInWork</p>
-				<p>/otherLinks</p>
-				<p>contact</p>
-			</div>
+		command = command.replace(/\s+/g, " ").trim().toUpperCase();
+		console.log(command)
+		if (command.split(' ')[0] == 'CD') {
+			cdCommand(command, output)
+			// if (command.split(' ')[1] == 'BIO'){
+				// 	output.innerHTML = `<p class="bio">
+				// 		<span style='font-weight: bold; color: #f600f6'> Shreyans Jain </span> | Shreyans13 <br>
+				// 		I am a student at <span style='color: #ffa500'>Chandigarh University</span> pursuing <span style='color: #4d4dff'>Bachelor of Technology</span> in Computer Science in Engineering.
+				// 		I <span style='color: #ff1a1a'>love coding</span> and i am <span style='color: #ff3333'>passionate</span> to learn <span 'style: color:#00ff9c'>new</span> things. 
+				// 		I also take part in various <span style='color: #00ccff'>coding competitions</span> and <span style='color: #00ccff'>hackathons.</span>
+				// 		<br>
+				// 		I may not be able to solve <strike>all</strike> <span style='color: #ffff4d'>some</span> of your problems.
+				// 	</p> ${commandBox}`
+				// } else if (command.split(' ')[1] == 'CERTIFICATES'){
+				// 	commandBox = commandHeader + `<span>/Certificates/</span>` + input 
+				// 	iC = true
+				// 	output.innerHTML = `${commandBox}`
+				// } else if (command.split(' ')[1] == 'SKILLS') {
 
-			</p> ${commandBox}`
+				// }else if (command.split(' ')[1] == '..'){
+				// 	commandBox = commandHeader + input 
+				// 	iC = false
+				// 	output.innerHTML = `${commandBox}`
+				// }
+				// else {
+				// 	output.innerHTML = `<p class="error">-portfolio: Directory: not found </p> ${commandBox}`	
+				// }
+		} else if (command == 'LS' || command == 'DIR') {
+			console.log(iC);
+			if (iC) {
+				output.innerHTML = `
+					<div class='certificates'>
+						<div class='' style='border: 3px solid white; padding: 20px;'>
+							<h3>The Web Developer Bootcamp by Udemy</h3>
+							<p>Issued May 2020</p>
+							<a href="">Verify Credential</a>
+						</div>
+						<div class='' style='border: 3px solid white; padding: 20px;'>	
+							<h3>Version Control with Git by Coursera</h3>
+							<p>Issued May 2020</p>
+							<a href="">Verify Credential</a>
+						</div>
+						<div class='' style='border: 3px solid white; padding: 20px;'>
+							<h3>Server Side Development using Node Js and MongoDB</h3>
+							<p>In Progress</p>
+						</div>
+					</div>
+
+				${commandBox}`
+				// window.scrollTo(0,document.body.scrollHeight);
+				// return
+			} else if (iP) { 
+				output.innerHTML = `
+				<div class='projects'>
+					<div class='' style='border: 3px solid white; padding: 20px;'>
+						<h3>Command Line Portfolio</h3>
+						<a href="">GitHub Repo</a>
+						<p> Deployed at <a href=''>GitHub Pages</a>
+					</div>
+					<div class='' style='border: 3px solid white; padding: 20px;'>
+						<h3>Command Line Portfolio</h3>
+						<a href="">GitHub Repo</a>
+						<p> Deployed at <a href=''>GitHub Pages</a>
+					</div>
+					<div class='' style='border: 3px solid white; padding: 20px;'>
+						<h3>Command Line Portfolio</h3>
+						<a href="">GitHub Repo</a>
+						<p> Deployed at <a href=''>GitHub Pages</a>
+					</div>
+				</div>
+				${commandBox}
+				`
+			} else if (iPW) {
+				output.innerHTML = `
+				<p class='ls'>
+					Besides learning new things I am presently working on the website for <span style='color: #ffa500'>Applied Engineering & Services</span>.
+					Its a local company focusing on sanitizing and services.
+					Technologies used in this project is <span style='color: #ffa500'>NODE JS</span> for backend and <span style='color: #ffa500'>BOOTSTRAP4</span> for frontend with an hidden dashboard.
+				</p>${commandBox}
+				`	
+			} else {
+				output.innerHTML = `<p class="ls">
+				<div style="color: #fffc58; padding-left: 45px; line-height: normal;">
+					<p>bio</p>
+					<p>/certificates</p>
+					<p>skills</p>
+					<p>/projects</p>
+					<p>/projectsInWork</p>
+					<p>/otherLinks</p>
+					<p>contact</p>
+				</div>
+
+				</p> ${commandBox}`
+			}
 		} else if (command == 'HELP' || command == 'INFO') {
 			output.innerHTML = `<p class="ls">
 			<span style="font-weight: bold;">PORTFOLIO POWERSHELL HELP SYSTEM</span><br> 
@@ -112,14 +293,25 @@ let caller = (event) => {
 				<p>DIR - To view all folders and files in the directory</p> 
 				<p>LS - To view all folders and files in the directory</p>
 				<p>CD - To change the current directory</p>
+				<p>REFRESH - To refresh the page</p>
 			</div>
 			</p> ${commandBox}`
+		} else if (command == 'REFRESH'){
+			location.reload()
+		} else if (command == 'CLS' || command == 'CLEARSCREEN') {
+			document.querySelector('.commandLine').innerHTML = `${commandBox}`
 		}
 		else {
 			output.innerHTML = `<p class="error">-portfolio: COMMAND: not found </p> ${commandBox}`
 			
 			// addName()
 		}
-		window.scrollTo(0,document.body.scrollHeight);
+		// window.scrollTo(0,document.body.scrollHeight);
+		let btn = document.querySelectorAll('#commandButton')
+		btn = btn[btn.length-1]
+		btn.focus()
+		console.log(btn)
 	}
+	window.scrollTo(0,document.body.scrollHeight);
+
 }
